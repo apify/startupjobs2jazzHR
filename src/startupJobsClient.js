b94d7757f06f0ed1c7845edc996b4fd6dc829625
HonzaTuron <jan.turon@apify.com>
2025-04-04 09:09:24 +0200

    application list

--- src/startupJobsClient.js
+++ src/startupJobsClient.js
@@ -35,7 +35,7 @@ export default class StartupJobsClient {
    * @returns {array} applications
    */
   async applicationList() {
-    const { data } = await api.get(`${this.url}/applications?created_at.gt=2025-04-01`, this.getConfig());
+    const { data } = await api.get(`${this.url}/applications?created_at.gt=2025-04-04`, this.getConfig());
     return data;
   }
 
