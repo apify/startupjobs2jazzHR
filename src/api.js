cf95c74b91f3fa4c47b0d7d3693d4dfb8e106922
Tomas Katz <katzino@gmail.com>
2026-01-07 14:56:10 +0100

    feat: add ashby retry logic (#4)

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
 import _ from 'underscore';
-import { log } from 'apify'
+import { log } from 'apify';
+
+const RETRY_FACTOR = 1000;
 
 const api = axios.create();
 
@@ -14,6 +17,17 @@ api.interceptors.request.use((request) => {
   }
   log.info('Starting request', logData);
   return request;
+});
+
+axiosRetry(api, {
+  retries: 5,
+  retryDelay: (retryCount, error) => axiosRetry.exponentialDelay(retryCount, error, RETRY_FACTOR),
+  retryCondition: (error) => {
+    return error.response?.status >= 500;
+  },
+  onRetry: (retryCount, error, requestConfig) => {
+    log.warning(`Retrying request to ${requestConfig.url} (attempt ${retryCount}/5) due to ${error.response?.status} error`);
+  },
 });
 
 export default api
